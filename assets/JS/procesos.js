476734bd4e530473681b0ccae3bbf9104f9761a2
pbucich <143009480+pbucich@users.noreply.github.com>
2023-11-06 11:09:49 -0300

    Add files via upload

--- assets/JS/procesos.js
+++ assets/JS/procesos.js
@@ -1,31 +1,36 @@
-var cantidad = document.getElementById("cant");
-var btnCalc = document.getElementById("resumen");
+var cantidad  = document.getElementById("exampleFormControlInput3");
+var btnCalc   = document.getElementById("resumen");
 var btnBorrar = document.getElementById("borrar");
 var categoria = document.getElementById("opt");
-var leyenda = document.getElementById("leyenda");
+var leyenda   = document.getElementById("Total");
 var formulario = document.getElementById("form");
 
-btnCalc.addEventListener("click", calcular);
-btnBorrar.addEventListener("click", limpiar);
+
+
+btnCalc.addEventListener("click",calcular);
+btnBorrar.addEventListener("click",limpiar);
 
 function calcular(){
     switch(categoria.value){
         case '1':
-         total=(parseInt(cantidad.value) * 200) * .2;
-         leyenda.innerText="valor del ticket: "+total;
-         break;
-         case '2':
-            total=(parseInt(cantidad.value) * 200) * .5;
-            leyenda.innerText="valor del ticket: "+total;
-            break;
+          total=(parseInt(cantidad.value)*200)*.2;
+          leyenda.innerText= total;
+          break;
+        case '2':
+          console.log("cantidad."+cantidad.value)
+          total=(parseInt(cantidad.value)*200)*.5;
+          leyenda.innerText=total;
+          break;
         case '3':
-            total=(parseInt(cantidad.value) * 200) * .85;
-            leyenda.innerText="valor del ticket: "+total;
-            break;
+          console.log("cantidad:"+cantidad.value)
+          total=(parseInt(cantidad.value)*200)*.85;
+          leyenda.innerText=total;
+          break;
 
     }
 }
 
 function limpiar(){
     formulario.reset();
+    leyenda.innerText="";
 }
